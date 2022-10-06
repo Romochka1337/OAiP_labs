93d4da2d21af6b3ff75bc2d54e73cb1a4385c705
Romochka1337 <104332558+Romochka1337@users.noreply.github.com>
2022-10-06 08:59:35 +0300

    lab3

---
+++ lab3/lab3_task1/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+int inputArraySize();
+void minElement(float arr[], int arrSize);
+void sumElements(float arr[], int arrSize);
+int main() {
+    int arrSize;
+    arrSize = inputArraySize();
+    float arr[100];
+    //input values to array
+    for(int i = 0; i<arrSize; i++)
+    {
+        printf("arr[%d] = ", i);
+        if(scanf("%f",&arr[i])!= 1){
+            i--;
+            rewind(stdin);
+        }
+    }
+    minElement(arr, arrSize);
+    sumElements(arr, arrSize);
+    return 0;
+}
+int inputArraySize(){
+    int arrSize;
+    printf("\nInput array size: ");
+    if(scanf("%d",&arrSize)!= 1 || arrSize <= 0){
+        printf("a");
+        rewind(stdin);
+        return inputArraySize();
+    }
+    else
+        return arrSize;
+}
+void minElement(float arr[], int arrSize){
+    float minValue = arr[0];
+    for (int i = 0; i < arrSize; i++) {
+        if(arr[i] < minValue){
+            minValue = arr[i];
+        }
+    }
+    printf("Minimal value is %f", minValue);
+}
+void sumElements(float arr[], int arrSize){
+    int amountPosNums = 0,indexFirstNum, indexLastNum;
+    float sum = 0;
+    for (int i = 0; i < arrSize; i++) {
+        if(arr[i] > 0){
+            amountPosNums++;
+        }
+    }
+    if (amountPosNums >= 2){
+        //find index of first positive number
+        for (int i = 0; i < arrSize; i++) {
+            if (arr[i] > 0){
+                indexFirstNum = i;
+                break;
+            }
+        }
+        //find index of last positive number
+        for (int i = arrSize-1; i >= 0; i--) {
+            if (arr[i] > 0){
+                indexLastNum = i;
+                break;
+            }
+        }
+        //count sum
+        for (int i = indexFirstNum+1; i < indexLastNum; i++) {
+            sum+= arr[i];
+        }
+        printf("\nSum = %f", sum);
+    } else{
+        printf("\nThere are not enough positive numbers in the array");
+    }
+}
+
