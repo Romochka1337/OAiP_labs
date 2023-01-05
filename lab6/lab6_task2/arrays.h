8ecb3f5e04cdf864d79e23a2534848c15a08e6e0
Romochka1337 <104332558+Romochka1337@users.noreply.github.com>
2023-01-05 13:22:00 +0300

    Delete lab6 directory

--- lab6/lab6_task2/arrays.h
+++
@@ -1,7 +0,0 @@
-int inputArraySize();
-void inputArrayElements(int **arr, int n, int m);
-void showArray(int **arr, int n, int m);
-void clearMemory(int **arr, int height);
-void countSums(int **arr, int height, int width);
-void mergeSort(int **arr, int start, int end, int width);
-
